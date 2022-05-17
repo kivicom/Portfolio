52d9db18ed225896ed8e29c20c4cf38178e67bb6
kiv-i <36578880+kivicom@users.noreply.github.com>
2022-05-17 12:09:38 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neon: "#52DEE5",
+        text: "#C7D3FF",
+        textDark: "#A1ABBF",
+        bgblue: "#222E50",
+        darkerBlue: "#0A192F",
+      },
+      fontFamily: {
+        sora: ["Sora", "sans-serif"],
+        fira: ["Fira Code", "monospace"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
